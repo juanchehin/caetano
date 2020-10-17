55050aae07ead3a6587721e96585d23a0af02938
Juan Chehin SGM <chehinjuan@gmail.com>
2020-10-17 19:11:51 -0300

    Creacion de formularios 2

--- CapaPresentacion/formNuevoEditarTurno.Designer.cs
+++ CapaPresentacion/formNuevoEditarTurno.Designer.cs
@@ -154,7 +154,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(917, 441);
+            this.ClientSize = new System.Drawing.Size(917, 382);
             this.Controls.Add(this.lblVariable);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
